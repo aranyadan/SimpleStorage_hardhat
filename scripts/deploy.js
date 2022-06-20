dba60118c69d3cbb06dae7f77be79dd763f4d622
Aranya Dan <aranya.dan1996@gmail.com>
2022-06-20 02:49:53 +0200

    Hardhat simplestorage tutorial by Patrick Collins

---
+++ scripts/deploy.js
@@ -0,0 +1,54 @@
+// imports
+const { ethers, run, network } = require("hardhat")
+// async main
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+    console.log(`Deployed contract to: ${simpleStorage.address}`)
+    // check if local environment
+    // console.log(network.config)
+
+    if (
+        (network.config.chainId === 4 || network.config.chainId === 5) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await simpleStorage.deployTransaction.wait(3)
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current vlaue is: ${currentValue}`)
+
+    // Update value
+    const transactionResponse = await simpleStorage.store(12)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
---
+++ test/test_deploy.js
@@ -0,0 +1,26 @@
+const { ethers } = require("hardhat")
+const { expect, assert } = require("chai")
+
+describe("SimpleStorage", () => {
+    let simpleStorageFactory, simpleStorage
+    beforeEach(async () => {
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+        simpleStorage = await simpleStorageFactory.deploy()
+    })
+
+    it("Should start with a favourite number of 0", async () => {
+        const currentValue = await simpleStorage.retrieve()
+        const expectedValue = "0"
+        assert.equal(currentValue.toString(), expectedValue)
+        // same but using expect
+        expect(currentValue.toString()).to.equal(expectedValue)
+    })
+
+    it("Should update when we call store", async () => {
+        const expectedValue = "7"
+        const transactionResponse = await simpleStorage.store(expectedValue)
+        await transactionResponse.wait(1)
+        const currentValue = await simpleStorage.retrieve()
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+})
